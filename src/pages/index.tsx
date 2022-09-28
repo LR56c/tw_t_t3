17530accf638d4415905d1f9ee20cd327a939318
Mauricio Herrera Ortega <ronalli56@gmail.com>
2022-09-27 23:47:19 -0300

    customsvgimage import fix2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { CustomSVGImage } from "../utils/CustomSVGImage";
+import { CustomSVGImage } from "../utils";
 
 const Home: NextPage = () =>
 {
